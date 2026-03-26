c231a0594bc3a922404e8a67c5daa18fc0fa0c1a
Gaz <tempe_bones@hotmail.com>
2026-03-26 01:27:05 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chromemory-permanent-v8';   // ← Increase this number every time you make changes
+const CACHE = 'chromemory-permanent-v9';   // ← Increase this number every time you make changes
 const BASE_PATH = '/chromemory';
 
 const ASSETS = [
@@ -14,7 +14,7 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e => {
-  console.log('📦 Installing Chromemory cache v7');
+  console.log('📦 Installing Chromemory cache v9');
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
@@ -22,7 +22,7 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  console.log('✅ Service worker v7 activated');
+  console.log('✅ Service worker v9 activated');
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
